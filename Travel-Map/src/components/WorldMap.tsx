36d0b6e3c8bf9001ff9c623dd9f90baef30589de
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-18 14:36:27 +0530

    create component as WorldMap

---
+++ Travel-Map/src/components/WorldMap.tsx
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+class WorldMap extends Component{
+state ={
+    render(){
+        return(
+            <div>
+                <h1>The World</h1>
+            </div>
+        );
+    }
+}
+}
+
+export default WorldMap;
