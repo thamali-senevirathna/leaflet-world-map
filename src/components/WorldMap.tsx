7250ded55c1c8fe3e6659075c28a058bae1ca509
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-19 13:14:36 +0530

    get counrties object in console

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MapContainer, GeoJSON , GeoJSONProps} from "react-leaflet";
+import { MapContainer, GeoJSON, FeatureGroup, LayerGroup} from "react-leaflet";
 import mapData from "./../data/countries.json";
 import "leaflet/dist/leaflet.css";
 import "./Worldmap.css";
@@ -12,16 +12,24 @@ class WorldMap extends Component {
     console.log(mapData);
   }
  countryStyle = {
-fillColor:"green"
-// fillOpacity: 1
+fillColor:"green",
+fillOpacity: 0.8,
+color:"black",
+weight:0.7,
+dashArray:1,
 };
+
+onEachCountry =(country: any, layer: any) =>{
+console.log(country);
+
+}
 
   render() {
     return (
       <div>
         <h1 style={{ textAlign: "center", fontSize: "1.1rem", marginTop: "0%", paddingBottom: "1%" }}>The Map</h1>
         <MapContainer style={{ height: "85vh", width: "90vw" }} zoom={2} center={[2, 100]}>
-          <GeoJSON style={this.countryStyle} data={mapData as GeoJSONData} />
+          <GeoJSON style={this.countryStyle} data={mapData as GeoJSONData} onEachFeature={this.onEachCountry} />
         </MapContainer>
       </div>
     );
