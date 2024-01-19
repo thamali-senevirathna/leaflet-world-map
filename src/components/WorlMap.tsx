bb736153b50bbb837115c82e4f71c17e0b9cee66
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-19 11:30:00 +0530

    import countries json file

--- src/components/WorlMap.tsx
+++ src/components/WorlMap.tsx
@@ -1,14 +1,20 @@
 import React,{Component } from "react";
 import { MapContainer } from "react-leaflet";
-import './../../node_modules/leaflet/dist/leaflet.css'
+import countries from './../data/countries.json';
+import './../../node_modules/leaflet/dist/leaflet.css';
+
 
 class WorlMap extends Component{
  state={};
+ componentDidMount(): void {
+     console.log(countries);
+     
+ }
     render() {
         return(
             <div>
-                <h1 >The Map</h1>
-                <MapContainer style={{height:"80vh", width:"100vw"}}></MapContainer>
+                <h1 style={{textAlign:"center",fontSize:"1.1rem",marginTop:"0%",paddingBottom:"1%"}}>The Map</h1>
+                <MapContainer style={{height:"85vh", width:"90vw"}} ></MapContainer>
             </div>
         );
     }
