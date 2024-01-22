b014c72189fa2880dd74c073f9b266cecca998a7
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-22 13:56:48 +0530

    remove click event ,added mouse event

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,27 +1,26 @@
-
-
-import React, { Component } from "react";
-import { MapContainer, GeoJSON, FeatureGroup, LayerGroup } from "react-leaflet";
-import mapData from "./../data/countries.json";
+import  { Component } from "react";
+import { MapContainer, GeoJSON } from "react-leaflet";
+import  mapData from "./../data/countries.json";
 import "leaflet/dist/leaflet.css";
 import "./Worldmap.css";
 
 interface GeoJSONData {
   features: any[];
 }
+
 class WorldMap extends Component {
 
-
-  state = { color:"#ffff00"};
+  state = { color: "#ffff00" };
 
   // color = ["#be2edd","#badc58","#3498db","#ffbe76","#6D214F"];
 
-
-  [x: string]: any;
-  componentDidMount() {
-    console.log(mapData);
-  }
-
+  onCountryMouseOver = (event: any){
+event.target.setStyle({
+  color:"#FC427B",
+  fillColor:this.state.color,
+  fillOpacity:1,
+})
+  };
   countryStyle = {
     fillColor: "green",
     fillOpacity: 0.8,
@@ -29,35 +28,14 @@ class WorldMap extends Component {
     weight: 0.7,
     dashArray: 1,
   };
-   onClickedCountryMassage = (event: any) => {
-    console.log( "Clicked..");
-   }
 
-   onCountryChangeColor = (event:any) =>{
-    event.target.setStyle({
-      color:"#FC427B",
-      fillColor:this.state.color,
-      fillOpacity:1,
-     })
-   }
-  onEachCountry = (country: any, layer: any) => {
-    const countryName = country.properties.ADMIN;
-    console.log(countryName);
-    layer.bindPopup(countryName);
+ 
 
-   layer.options.fillOpacity = Math.random();
-//   const colorIndex = Math.floor(Math.random() * this.color.length);
-// layer.options.fillColor = this.color[colorIndex];
 
-    layer.on({
-    click: this.onCountryChangeColor,
-    });
+
+  colorChange = (event: any) => {
+    this.setState({ color: event.target.value });
   };
-
-colorChange = (event:any) =>{
-this.setState({color: event.target.value})
-}
-
 
   render() {
     return (
@@ -66,27 +44,42 @@ this.setState({color: event.target.value})
           style={{
             textAlign: "center",
             fontSize: "1rem",
-            position:"absolute",
-marginTop:"-21%"
+            position: "absolute",
+            marginTop: "-21%",
           }}
         >
           The Map
         </h1>
+   
         <MapContainer
-          style={{ height: "85vh", width: "85vw",right:"8%",position:"absolute",bottom:"6%" }}
+          style={{
+            height: "85vh",
+            width: "85vw",
+            right: "8%",
+            position: "absolute",
+            bottom: "6%",
+          }}
           zoom={2}
           center={[2, 100]}
         >
+       
           <GeoJSON
             style={this.countryStyle}
             data={mapData as GeoJSONData}
             onEachFeature={this.onEachCountry}
           />
         </MapContainer>
-        <input type="color" value={this.state.color} onChange={this.colorChange} style={{position:"absolute",bottom:"3%",left:"0.99%"}}/>
+
+        <input
+          type="color"
+          value={this.state.color}
+          onChange={this.colorChange}
+          style={{ position: "absolute", bottom: "3%", left: "0.99%" }}
+        />
       </div>
     );
   }
 }
 
+// Exporting the WorldMap component as the default export
 export default WorldMap;
